7575011b5f17d0f2e0c03409b29351f341960c74
Yuri Soens <info.antwerpflats@gmail.com>
2026-08-28 13:04:58 +0200

    Switch to static export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    // Wix-hosted photos for now, per plan; swap to local /public images later
-    // and this remotePatterns entry can be removed.
-    remotePatterns: [
-      { protocol: 'https', hostname: 'static.wixstatic.com' },
-    ],
-  },
-};
-
 module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */ 
+const nextConfig = { 
+	output: 'export', trailingSlash: true, images: { remotePatterns: [ { protocol: 'https', hostname: 'static.wixstatic.com' }, ], }, }; module.exports = nextConfig;
