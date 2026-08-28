cecba609ea7f2355b25bfec1a3e6940d43031bf3
Yuri Soens <info.antwerpflats@gmail.com>
2026-08-28 13:14:40 +0200

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,1 @@
-module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */ 
-const nextConfig = { 
-	output: 'export', trailingSlash: true, images: { remotePatterns: [ { protocol: 'https', hostname: 'static.wixstatic.com' }, ], }, }; module.exports = nextConfig;
+/** @type {import('next').NextConfig} */ const nextConfig = { output: 'export', trailingSlash: true, images: { remotePatterns: [ { protocol: 'https', hostname: 'static.wixstatic.com' }, ], }, }; module.exports = nextConfig;
